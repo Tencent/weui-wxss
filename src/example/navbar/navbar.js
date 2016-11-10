35f73e76fea43b497ae0a3c31c6dc2f31c43e94e
bearyan <bearverygood@126.com>
2016-11-10 20:12:46 +0800

    v0.1.1 [修复] navbar的样式按小程序的规范来重新编写

--- src/example/navbar/navbar.js
+++ src/example/navbar/navbar.js
@@ -1,10 +1,25 @@
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
     data: {
         tabs: ["选项一", "选项二", "选项三"],
-        activeIndex: "0"
+        activeIndex: "0",
+        sliderOffset: 0,
+        sliderLeft: 0
+    },
+    onLoad: function () {
+        var that = this;
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2
+                });
+            }
+        });
     },
     tabClick: function (e) {
         this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
             activeIndex: e.currentTarget.id
         });
     }
