a62146258e37f96ffb0d7b631b08224646af0070
bearyan <bearverygood@126.com>
2017-01-05 15:30:26 +0800

    fix navbar的activeIndex BUG #20

--- src/example/navbar/navbar.js
+++ src/example/navbar/navbar.js
@@ -3,7 +3,7 @@ var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 Page({
     data: {
         tabs: ["选项一", "选项二", "选项三"],
-        activeIndex: "0",
+        activeIndex: 1,
         sliderOffset: 0,
         sliderLeft: 0
     },
@@ -12,7 +12,8 @@ Page({
         wx.getSystemInfo({
             success: function(res) {
                 that.setData({
-                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
                 });
             }
         });
