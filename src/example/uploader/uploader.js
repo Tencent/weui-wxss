ed181ccaa707d1b3c207b42df5d62a2941e3f115
xushengni <xushengni@tencent.com>
2023-12-15 16:30:22 +0800

    feat: v2.6.8

--- src/example/uploader/uploader.js
+++ src/example/uploader/uploader.js
@@ -3,23 +3,4 @@ Page({
   data: {
     files: [],
   },
-  chooseImage() {
-    const that = this;
-    wx.chooseImage({
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success(res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          files: that.data.files.concat(res.tempFilePaths),
-        });
-      },
-    });
-  },
-  previewImage(e) {
-    wx.previewImage({
-      current: e.currentTarget.id, // 当前显示图片的http链接
-      urls: this.data.files, // 需要预览的图片http链接列表
-    });
-  },
 });
